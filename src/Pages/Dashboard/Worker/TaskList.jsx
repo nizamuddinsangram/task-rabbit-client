60a1e1232b6c2bfa2ea2bcb577871dec6cad63fc
Nizam uddin Sangram <nizamuddinstruggle@gmail.com>
2024-06-03 01:03:07 +0600

    show tasklist data

--- src/Pages/Dashboard/Worker/TaskList.jsx
+++ src/Pages/Dashboard/Worker/TaskList.jsx
@@ -1,7 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import TaskListCard from "../../../components/Dashboard/WorkerC/TaskListCard";
+import useAxiosCommon from "../../../hooks/useAxiosCommon";
+
 const TaskList = () => {
+  const axiosCommon = useAxiosCommon();
+  const { data: taskList, refetch } = useQuery({
+    queryKey: ["taskList"],
+    queryFn: async () => {
+      const { data } = await axiosCommon(`/tasks`);
+      return data;
+    },
+  });
+  console.log(taskList);
   return (
-    <div>
-      <p>this is my taskList </p>
+    <div className="grid grid-cols-1 gap-20 lg:grid-cols-3 md:grid-cols-1">
+      {taskList.map((task) => (
+        <TaskListCard task={task} key={task._id} />
+      ))}
     </div>
   );
 };
--- src/components/Dashboard/WorkerC/TaskListCard.jsx
+++ src/components/Dashboard/WorkerC/TaskListCard.jsx
@@ -1,16 +1,21 @@
-const TaskListCard = () => {
+const TaskListCard = ({ task }) => {
+  console.log(task);
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
-        <img src="" alt="Shoes" />
+        <img src={task.task_image_url} alt="Shoes" />
       </figure>
-      <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white"></p>
+      <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">
+        {task.task_title}
+      </p>
       <div className="card-body flex flex-col items-center">
-        <h2 className="card-title"></h2>
-        <p></p>
+        <h2 className="card-title">{task.task_creator.creator_name}</h2>
+        <p>{task.completion_date}</p>
+        <p>{task.payable_amount}</p>
+        <p>{task.task_quantity}</p>
         <div className="card-actions justify-end">
           <button className="btn mb-2 btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">
-            ADD TO CART
+            View Details
           </button>
         </div>
       </div>
