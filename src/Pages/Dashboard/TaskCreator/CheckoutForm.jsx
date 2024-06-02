ec742ef205fcadb9a3bd8bafc0f110423f02618f
Nizam uddin Sangram <nizamuddinstruggle@gmail.com>
2024-06-02 23:19:36 +0600

    update coins value when you buy coins

--- src/Pages/Dashboard/TaskCreator/CheckoutForm.jsx
+++ src/Pages/Dashboard/TaskCreator/CheckoutForm.jsx
@@ -2,8 +2,10 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosCommon from "../../../hooks/useAxiosCommon";
+import useRole from "../../../hooks/useRole";
 
 const CheckoutForm = ({ amount }) => {
+  const [, refetch] = useRole();
   const { user } = useAuth();
   const [error, setError] = useState("");
   const [clientSecret, setClientSecret] = useState("");
@@ -71,6 +73,18 @@ const CheckoutForm = ({ amount }) => {
       if (paymentIntent.status === "succeeded") {
         console.log("taka keta nise", paymentIntent.id);
         setTransactionId(paymentIntent.id);
+        //post request start
+        const { data } = await axiosCommon.post("/payment-info", {
+          email: user.email,
+          amount: amount,
+          transactionId: paymentIntent.id,
+        });
+        if (data.updatedResult.modifiedCount > 0) {
+          refetch();
+          console.log(data.updatedResult.modifiedCount);
+        }
+
+        //post request end
       }
     }
   };
